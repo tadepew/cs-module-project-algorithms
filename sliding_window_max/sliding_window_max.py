4511f5f9ecae4d03898de9859ba9670519fdc6ba
Tristan Depew <tristan.depew@gmail.com>
2020-05-20 19:17:51 -0400

    finished window

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,15 +2,38 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
-def sliding_window_max(nums, k):
-    # Your code here
+import sys
+INT_MIN = -sys.maxsize - 1
 
-    pass
+
+def sliding_window_max(nums, k):
+
+    # length must be greater than k
+    if not len(nums) > k:
+        print("Invalid")
+        return -1
+
+    # Find of first window of size k
+    max_sum = INT_MIN
+    window_max = []
+    window_max.append(max(nums[:k]))
+
+    # Compute sums of remaining windows by
+    # removing first element of previous
+    # window and adding last element of
+    # current window.
+    for i in range(1, len(nums)-k+1):
+        new_window_right = i + k
+        new_window_left = i
+        window_max.append(max(nums[new_window_left:new_window_right]))
+
+    return window_max
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
